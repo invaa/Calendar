8420a2a02cd11ffbdd6845ab997a6f009abd9e49
ivartanian <igor.vartanian@gmail.com>
2014-09-07 14:44:26 +0300

    add package test and class testDataStore

---
+++ service/src/com/diosoft/trsine/calendar/test/datastore/DataStoreTest.java
@@ -0,0 +1,13 @@
+package com.diosoft.trsine.calendar.test.datastore;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DataStoreTest {
+
+    @Test
+    public void testDataStore(){
+        Assert.assertTrue(true);
+    }
+
+}
