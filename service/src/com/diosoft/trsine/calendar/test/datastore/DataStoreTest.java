48d3bcdd69eb578c0e50df276b41916e003ad084
ivartanian <igor.vartanian@gmail.com>
2014-09-07 17:47:04 +0300

    rename from DataStore to DataStoreTest

--- service/src/com/diosoft/trsine/calendar/test/datastore/DataStoreTest.java
+++ service/src/com/diosoft/trsine/calendar/test/datastore/DataStoreTest.java
@@ -3,7 +3,7 @@ package com.diosoft.trsine.calendar.test.datastore;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class DataStore {
+public class DataStoreTest {
 
     @Test
     public void testDataStore(){
