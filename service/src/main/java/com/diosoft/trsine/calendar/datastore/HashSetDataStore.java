79ef2a5c536d251f7b48cffe85eaff76f7f9fa14
invaa <alex@zamkovyi.name>
2014-09-09 10:37:06 +0300

    added HashSetDataStore

---
+++ service/src/main/java/com/diosoft/trsine/calendar/datastore/HashSetDataStore.java
@@ -0,0 +1,51 @@
+package com.diosoft.trsine.calendar.datastore;
+
+import com.diosoft.trsine.calendar.common.Event;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class HashSetDataStore implements DataStore {
+
+//    Map<UUID,Event>
+//    Map<String, List<UUID>>
+//    Map<Date, List<UUID>>
+    ArrayList<Event> events = new ArrayList<>();
+
+    @Override
+    public void add(Event event) {
+        events.add(event);
+    }
+
+    @Override
+    public void addAll(Collection<Event> events) {
+        events.addAll(events);
+    }
+
+    @Override
+    public void remove(UUID id) {
+        events.removeAll(events.parallelStream()
+                .filter(p -> p.getId().equals(id)).collect(Collectors.toList()));
+    }
+
+    @Override
+    public List<Event> searchByDescription(String description) {
+        return events.parallelStream()
+                .filter(p -> p.getDescription().equals(description))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Event> searchByTitle(String title) {
+        return events.parallelStream()
+                .filter(p -> p.getTitle().equals(title))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Event> searchByDay(Date day) {
+        return events.parallelStream()
+                .filter(p -> p.getDateBegin().equals(day))
+                .collect(Collectors.toList());
+    }
+}
