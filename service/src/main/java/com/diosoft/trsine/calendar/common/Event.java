b89f242ba766d91261f08a809210f587b7fe2242
ivartanian <igor.vartanian@gmail.com>
2014-09-11 01:47:57 +0300

    new branch

--- service/src/main/java/com/diosoft/trsine/calendar/common/Event.java
+++ service/src/main/java/com/diosoft/trsine/calendar/common/Event.java
@@ -27,7 +27,7 @@ public class Event {
     //clone objects
     protected final Cloner cloner = new Cloner();
 
-    private Event(Builder builder) {
+    protected Event(Builder builder) {
         this.description = builder.description;
         this.attenders = builder.attenders;
         this.dateBegin = builder.dateBegin;
--- service/src/test/java/com/diosoft/trsine/calendar/datastore/DataStoreTest.java
+++ service/src/test/java/com/diosoft/trsine/calendar/datastore/DataStoreTest.java
@@ -1,21 +1,40 @@
 package com.diosoft.trsine.calendar.datastore;
 
 import com.diosoft.trsine.calendar.common.Event;
+import com.diosoft.trsine.calendar.common.TestEventChield;
+import com.diosoft.trsine.calendar.comparison.ComparatorDateBegin;
 import com.diosoft.trsine.calendar.exceptions.IncorrectPeriodDates;
 import com.diosoft.trsine.calendar.service.CalendarServiceImp;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class DataStoreTest {
 
     @Test
     public void testDataStore() throws IncorrectPeriodDates {
-        Event testEvent = new Event.Builder() {
+
+//        Event testEvent = new Event.Builder() {
+//            @Override
+//            public Set newSet() {
+//                return new HashSet<String>();
+//            }
+//        }//end of Builder implementation
+//                .setDateBegin(new Date())
+//                .setDateEnd(new Date())
+//                .setTitle("Daily Scrum")
+//                .setDescription("Next daily scrum meeting")
+//                .addAttender("alex@zamkovyi.name")
+//                .addAttender("igor.vartanian@gmail.com")
+//                .build();
+//
+//        DataStore dataStore = new DataStoreImp();
+//        CalendarServiceImp service = new CalendarServiceImp(dataStore);
+//        service.add(testEvent);
+
+
+        Event.Builder builder = new Event.Builder() {
             @Override
             public Set newSet() {
                 return new HashSet<String>();
@@ -26,12 +45,20 @@ public class DataStoreTest {
                 .setTitle("Daily Scrum")
                 .setDescription("Next daily scrum meeting")
                 .addAttender("alex@zamkovyi.name")
-                .addAttender("igor.vartanian@gmail.com")
-                .build();
+                .addAttender("igor.vartanian@gmail.com");
+        Event testEvent = builder.build();
 
         DataStore dataStore = new DataStoreImp();
         CalendarServiceImp service = new CalendarServiceImp(dataStore);
         service.add(testEvent);
+        TestEventChield testEvent2 = new TestEventChield(builder);
+
+
+        ComparatorDateBegin compar = new ComparatorDateBegin();
+        compar.compare(testEvent, testEvent2);
+
+
+
     }
 
 }
