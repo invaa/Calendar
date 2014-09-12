ee6f1a54bf5e700f462efd8c6c7dd5e75f3cdb98
ivartanian <igor.vartanian@gmail.com>
2014-09-12 22:19:42 +0300

    Test

--- service/src/test/java/com/diosoft/trsine/calendar/datastore/DataStoreTest.java
+++ service/src/test/java/com/diosoft/trsine/calendar/datastore/DataStoreTest.java
@@ -1,13 +1,74 @@
 package com.diosoft.trsine.calendar.datastore;
 
+import com.diosoft.trsine.calendar.common.Event;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.*;
+
 public class DataStoreTest {
 
-    @Test
-    public void testDataStore(){
-        Assert.assertTrue(true);
+//    OrganizerUtil util = new OrganizerUtil();
+//
+//    @Test
+//    public void testDataStore() throws IncorrectPeriodDates {
+//
+//        DataStore ds = new DataStoreImp();
+//
+//        Set<String> attenders = util.createAttenders(5, "user");
+//        Date date = new GregorianCalendar(2014, 10, 12, 10, 0, 0).getTime();
+//
+//        Map<UUID, Event> currentDataStore = ds.getEventsMap();
+//
+//        //resultHelpMap
+//        Map<UUID, Event> resultHelpMap = new HashMap<>();
+//
+//
+//        //add
+//        for (int i = 0; i < 3; i++) {
+//            Event eventAdd = util.createEvent(i, date, date, String.valueOf(i) + "_" + "Event",
+//                    String.valueOf(i) + "_" + "DescriptionEvent", attenders);
+//            ds.add(eventAdd);
+//            resultHelpMap.put(eventAdd.getId(), eventAdd);
+//        }
+//        currentDataStore = ds.getEventsMap();
+//        Assert.assertEquals(resultHelpMap, currentDataStore);
+//
+//        //remove
+////        currentDataStore.
+////        resultHelpMap.remove();
+////        ds.remove(eventRemove.getId());
+//
+//        //addAll
+//        HashSet<Event> hashEvents = new HashSet<>();
+//        for (int i = 3; i < 7; i++) {
+//            Event eventAddAll = util.createEvent(i, date, date, String.valueOf(i) + "_" + "Event",
+//                    String.valueOf(i) + "_" + "DescriptionEvent", attenders);
+//            hashEvents.add(eventAddAll);
+//        }
+//        ds.addAll(hashEvents);
+//
+//        //searchByDescription
+//        List<Event> resultSearchByDescription = new ArrayList<>();
+//        resultSearchByDescription.add(util.createEvent(3, date, date, String.valueOf(3) + "_" + "Event",
+//                String.valueOf(3) + "_" + "DescriptionEvent", attenders));
+//        resultSearchByDescription.add(util.createEvent(3, date, date, String.valueOf(3) + "_" + "Event",
+//                String.valueOf(3) + "_" + "DescriptionEvent", attenders));
+//
+//        List<Event> searchByDescription = ds.searchByDescription("3_DescriptionEvent");
+//        Assert.assertEquals(resultSearchByDescription, searchByDescription);
+//
+//        //searchByTitle
+//        List<Event> resultSearchByTitle = new ArrayList<>();
+//        resultSearchByTitle.add(util.createEvent(2, date, date, String.valueOf(2) + "_" + "Event",
+//                String.valueOf(2) + "_" + "DescriptionEvent", attenders));
+//
+//        List<Event> searchDescription = ds.searchByDescription("2_DescriptionEvent");
+//        Assert.assertEquals(resultSearchByDescription, searchDescription);
+//
+//
+//
+
     }
 
 }
