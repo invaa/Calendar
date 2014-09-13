53781b6d08c7d90538d70ad7b82dfa811c31c671
ivartanian <igor.vartanian@gmail.com>
2014-09-13 18:15:39 +0300

    new branch

--- service/src/test/java/com/diosoft/trsine/calendar/datastore/HashMapDataStoreTest.java
+++ service/src/test/java/com/diosoft/trsine/calendar/datastore/HashMapDataStoreTest.java
@@ -2,22 +2,21 @@ package com.diosoft.trsine.calendar.datastore;
 
 import com.diosoft.trsine.calendar.common.Event;
 import com.diosoft.trsine.calendar.exceptions.IncorrectPeriodDates;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
 import java.util.*;
 
 public class HashMapDataStoreTest {
 
-    int indexTest = 400000;
+    int indexTest = 2;
 
     OrganizerUtil util = new OrganizerUtil();
-    DataStore ds = new HashMapDataStoreImp();
+    DataStore dsAdd = new HashMapDataStoreImp();
+    DataStore dsAddAll = new HashMapDataStoreImp();
 
     //expectedHelpMap
-    Map<UUID, Event> resultHelpMap = new HashMap<>();
+    Map<UUID, Event> resultHelpMapAdd = new HashMap<>();
+    Map<UUID, Event> resultHelpMapAddAll = new HashMap<>();
 
     //this event will be remove, search
     Event rememberEvent;
@@ -29,122 +28,146 @@ public class HashMapDataStoreTest {
     HashSet<Event> hashEvents = new HashSet<>();
 
     //result searchByDescription
-    List<Event> resultSearchByDescription = new ArrayList<>();
+    List<Event> resultSearchByDescription;
 
     //result searchByTitle
-    List<Event> resultSearchByTitle = new ArrayList<>();
+    List<Event> resultSearchByTitle;
 
     //result searchByDate
-    List<Event> resultSearchByDate = new ArrayList<>();
+    List<Event> resultSearchByDate;
 
     @Before
     public void fillData() throws IncorrectPeriodDates {
 
         Date date = new GregorianCalendar(2014, 10, 12, 10, 0, 0).getTime();
-        HashSet<String> attenders = new HashSet<>();
 
-        for (int i = 0; i <= 10; i++) {
-            attenders.add(String.valueOf(i) + "_" + "User");
-        }
-        for (int i = 0; i < 1000000; i++) {
-            Event eventAdd = util.createEvent(i, date, date, String.valueOf(i) + "_" + "Event",
-                    String.valueOf(i) + "_" + "DescriptionEvent", attenders);
-            resultHelpMap.put(eventAdd.getId(), eventAdd);
-            ds.add(eventAdd);
+        Set<String> attenders = util.createAttenders(10, "User");
+
+        Event eventAdd0 = util.createEvent(0, date, date, "Event", "DescriptionEvent", attenders);
+        Event eventAdd1 = util.createEvent(1, date, date, "Event", "DescriptionEvent", attenders);
+        Event eventAdd2 = util.createEvent(2, date, date, "Event", "DescriptionEvent", attenders);
+        Event eventAdd3 = util.createEvent(1, date, date, "Event", "DescriptionEvent", attenders);
+        Event eventAdd4 = util.createEvent(3, date, date, "Event", "DescriptionEvent", attenders);
+        Event eventAdd5 = util.createEvent(4, date, date, "Event", "DescriptionEvent", attenders);
+
+        //data for Add
+        for (int i = 0; i < 2; i++) {
+            Event eventAdd = util.createEvent(i, date, date, "Event", "DescriptionEvent", attenders);
+            resultHelpMapAdd.put(eventAdd.getId(), eventAdd);
+            dsAdd.add(eventAdd);
             if (i == indexTest) {
                 rememberEvent = eventAdd;
             }
-            //expected SearchByDescription
-            if (eventAdd.getDescription() == String.valueOf(indexTest) + "_" + "DescriptionEvent") {
-                resultSearchByDescription.add(eventAdd);
-            }
-            //expected SearchByTitle
-            if (eventAdd.getTitle() == String.valueOf(indexTest) + "_" + "Event") {
-                resultSearchByTitle.add(eventAdd);
-            }
-            //expected SearchByDate
-            if (eventAdd.getDateBegin() == date) {
-                resultSearchByDate.add(eventAdd);
-            }
+            addResultSearch(date, eventAdd);
+
         }
 
         //data for addAll
-        for (int i = 0; i < 500000; i++) {
-            Event eventAddAll = util.createEvent(i, date, date, String.valueOf(i) + "_" + "Event",
-                    String.valueOf(i) + "_" + "DescriptionEvent", attenders);
+        for (int i = 0; i < 1; i++) {
+
+            Event eventAddAll = util.createEvent(i, date, date, "Event", "DescriptionEvent", attenders);
             hashEvents.add(eventAddAll);
-            resultHelpMap.put(eventAddAll.getId(), eventAddAll);
-            //expected SearchByDescription
-            if (eventAddAll.getDescription() == String.valueOf(indexTest) + "_" + "DescriptionEvent") {
-                resultSearchByDescription.add(eventAddAll);
-            }
-            //expected SearchByTitle
-            if (eventAddAll.getTitle() == String.valueOf(indexTest) + "_" + "Event") {
-                resultSearchByTitle.add(eventAddAll);
-            }
-            //expected SearchByDate
-            if (eventAddAll.getDateBegin() == date) {
-                resultSearchByDate.add(eventAddAll);
-            }
+            resultHelpMapAddAll.put(eventAddAll.getId(), eventAddAll);
+
+            addResultSearch(date, eventAddAll);
 
         }
 
+    }
+
+    private void addResultSearch(Date date, Event eventAdd) {
+
+        //expected SearchByDescription
+        if (eventAdd.getDescription() == String.valueOf(indexTest) + "_" + "DescriptionEvent") {
+            if (resultSearchByDescription == null) {
+                resultSearchByDescription = new ArrayList<>();
+            }
+
+            resultSearchByDescription.add(eventAdd);
+        }
+        //expected SearchByTitle
+        if (eventAdd.getTitle() == String.valueOf(indexTest) + "_" + "Event") {
+            if (resultSearchByTitle == null) {
+                resultSearchByTitle = new ArrayList<>();
+            }
+            resultSearchByTitle.add(eventAdd);
+        }
+        //expected SearchByDate
+        if (eventAdd.getDateBegin() == date) {
+            if (resultSearchByDate == null) {
+                resultSearchByDate = new ArrayList<>();
+            }
+
+            resultSearchByDate.add(eventAdd);
+        }
     }
 
     @After
     public void clearData() {
-        ds = new HashMapDataStoreImp();
-        resultHelpMap = new HashMap<>();
+        dsAdd = new HashMapDataStoreImp();
+        dsAddAll = new HashMapDataStoreImp();
+        resultHelpMapAdd = new HashMap<>();
+        resultHelpMapAddAll = new HashMap<>();
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testAddDataStore() {
 
-        currentDataStore = ds.getDataStore();
-        Assert.assertEquals(resultHelpMap, currentDataStore);
+        currentDataStore = dsAdd.getDataStore();
+        Assert.assertEquals(resultHelpMapAdd, currentDataStore);
 
     }
 
-    @Test(timeout = 500)
+    @Test(timeout = 5000)
     public void testAddAllDataStore() {
 
-        ds.addAll(hashEvents);
-        currentDataStore = ds.getDataStore();
-        Assert.assertEquals(resultHelpMap, currentDataStore);
+        dsAddAll.addAll(hashEvents);
+        currentDataStore = dsAddAll.getDataStore();
+        Assert.assertEquals(resultHelpMapAddAll, currentDataStore);
 
     }
 
-    @Test(timeout = 10)
+    @Ignore
+    @Test(timeout = 1000)
     public void testRemoveDataStore() {
 
-        ds.remove(rememberEvent.getId());
-        resultHelpMap.remove(rememberEvent.getId());
-        currentDataStore = ds.getDataStore();
-        Assert.assertEquals(resultHelpMap, currentDataStore);
+        if (rememberEvent == null){
+            Assert
+        }
+
+        dsAdd.remove(rememberEvent.getId());
+        resultHelpMapAdd.remove(rememberEvent.getId());
+        currentDataStore = dsAdd.getDataStore();
+        Assert.assertEquals(resultHelpMapAdd, currentDataStore);
 
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testSearchByDescriptionDataStore() {
 
-        List<Event> searchByDescription = ds.searchByDescription(String.valueOf(indexTest) + "_" + "DescriptionEvent");
-        Assert.assertEquals(resultSearchByDescription, searchByDescription);
+        List<Event> searchByDescription = dsAdd.searchByDescription(String.valueOf(indexTest) + "_" + "DescriptionEvent");
+        if (resultSearchByDescription == null) {
+            Assert.assertTrue(resultSearchByDescription == searchByDescription);
+        }else{
+            Assert.assertEquals(resultSearchByDescription, searchByDescription);
+        }
+
 
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testSearchByTitleDataStore() {
 
-        List<Event> resultSearchByTitle = ds.searchByTitle(String.valueOf(indexTest) + "_" + "Event");
+        List<Event> resultSearchByTitle = dsAdd.searchByTitle(String.valueOf(indexTest) + "_" + "Event");
         Assert.assertEquals(resultSearchByTitle, resultSearchByTitle);
 
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testSearchByDateDataStore() {
 
         Date dateSearch = new GregorianCalendar(2014, 10, 12, 10, 0, 0).getTime();
-        List<Event> resultSearchByDate = ds.searchByDay(dateSearch);
+        List<Event> resultSearchByDate = dsAdd.searchByDay(dateSearch);
         Assert.assertEquals(resultSearchByDate, resultSearchByDate);
 
     }
